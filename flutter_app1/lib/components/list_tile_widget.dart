ede1d0e62b3be0959a6a476dbe4ab4cbb80e27eb
Baihaki Kuncara <baihakidwi@gmail.com>
2023-09-05 21:15:14 +0700

    - put list view on card

--- flutter_app1/lib/components/list_tile_widget.dart
+++ flutter_app1/lib/components/list_tile_widget.dart
@@ -30,49 +30,51 @@ class ListTileWidget extends StatelessWidget {
           ),
         );
       },
-      child: Container(
-        padding: const EdgeInsets.all(4),
-        child: Row(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 50,
-              height: 50,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/logo.png'),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
+      child: Card(
+        child: Container(
+          padding: const EdgeInsets.all(4),
+          child: Row(
+            children: [
+              Container(
                 padding: const EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      name,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    Text(
-                      description,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.labelMedium,
-                    ),
-                  ],
+                width: 50,
+                height: 50,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/logo.png'),
+                  ),
                 ),
               ),
-            ),
-            IconButton(
-              color: Theme.of(context).colorScheme.primary,
-              onPressed: () {},
-              icon: favorite
-                  ? Icon(Icons.favorite)
-                  : Icon(Icons.favorite_outline),
-            ),
-          ],
+              Expanded(
+                flex: 1,
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      Text(
+                        description,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              IconButton(
+                color: Theme.of(context).colorScheme.primary,
+                onPressed: () {},
+                icon: favorite
+                    ? Icon(Icons.favorite)
+                    : Icon(Icons.favorite_outline),
+              ),
+            ],
+          ),
         ),
       ),
     );
